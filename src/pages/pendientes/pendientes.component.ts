88efa8d6cb625b55d8614898d17fa7bc0e283a2a
Diego Warthon <diegowarthon1190@gmail.com>
2018-06-10 14:48:23 -0500

    Actualizamos a ultimo pendiente

---
+++ src/pages/pendientes/pendientes.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+
+	selector: 'app-pendientes',
+	templateUrl: 'pendientes.component.html'
+})
+export class PendientesComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit() {
+		
+	}
+}
